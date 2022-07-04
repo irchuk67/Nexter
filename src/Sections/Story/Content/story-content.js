3b2678c2073810b10adb02f5a7ac9922bca707c6
irchuk67 <foxmaster110518@gmail.com>
2022-07-05 00:38:20 +0300

    first commit

---
+++ src/Sections/Story/Content/story-content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './story-pictures.scss';
+
+let StoryPictures = () => {
+    return (
+        <div className="story__pictures">
+            story pictures
+        </div>
+    )
+}
+
+export default StoryPictures;
